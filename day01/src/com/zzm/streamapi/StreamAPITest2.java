2c039f5a6dfb88d0acff9552528d6daf8a21f50a
MingsonZheng <mingsonzheng@outlook.com>
2023-12-27 15:30:51 +0800

    Stream的终止操作-收集

--- day01/src/com/zzm/streamapi/StreamAPITest2.java
+++ day01/src/com/zzm/streamapi/StreamAPITest2.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -105,5 +107,20 @@ public class StreamAPITest2 {
         // Optional<Double> reduce1 = salaryStream.reduce(Double::sum);
         Optional<Double> reduce1 = salaryStream.reduce((d1, d2) -> d1 + d2);
         System.out.println("reduce1=" + reduce1);
+    }
+
+    // 3-收集
+    @Test
+    public void test4() {
+        // collect(Collector c)将流转换为其他形式。接放一个Collector接口的实现，用于给Stream中元素做汇总的方法
+        // 练习1:查找工资大于6000的员工，结果返回为一个List或Set
+        List<Employee> employees = EmployeeData.getEmployees();
+        List<Employee> employeeList = employees.stream().filter(e -> e.getSalary() > 6000).collect(Collectors.toList());
+        employeeList.forEach(System.out::println);
+
+        System.out.println("**********************");
+
+        Set<Employee> employeeSet = employees.stream().filter(e -> e.getSalary() > 6000).collect(Collectors.toSet());
+        employeeSet.forEach(System.out::println);
     }
 }
